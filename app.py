6c9c60ab63277eabb0a5baa5611bab5eec1bfb7c
Andre Figueiredo <afigueiredo@onesource.pt>
2018-04-21 00:35:58 +0100

    insert, list user

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from firebase import firebase
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import json
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+firebase1 = firebase.FirebaseApplication('https://tinderdogs-998c1.firebaseio.com', authentication=None)
 
 
-@app.route("/create_user/<name>")
-def hello(name):
-    # create user
-    # insert na bd
-    # retun json
-    return "o utilizador " + name + " foi criado"
+@app.route("/create_user", methods=['POST'])
+def create_user():
+
+    data = {'name': request.form.get('name'), 'city': request.form.get('city')}
+    firebase1.post("/users/", data)
+
+    return str(uuid.uuid1())
+
+@app.route("/list_users")
+def list_users():
+
+    result = firebase1.get('/users',None)
+    return str(result.keys())
 
 
 if __name__ == "__main__":
