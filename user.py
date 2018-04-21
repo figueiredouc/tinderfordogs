45d52e862e4ff4b25fba1d06d89b1e46678ac790
Andre Figueiredo <afigueiredo@onesource.pt>
2018-04-21 15:18:04 +0100

    update

---
+++ user.py
@@ -0,0 +1,19 @@
+from config import fb
+
+
+class User:
+    def __init__(self, r):
+        self.name = r.form.get('name')
+        self.latitude = r.form.get('latitude')
+        self.latitude = r.form.get('latitude')
+        self.city = r.form.get('city')
+        self.email = r.form.get('email')
+        self.isbreeder = r.form.get('isbreeder')
+        fb.post("/users/", self.__dict__)
+
+    @staticmethod
+    def find_my_dogs():
+        dogs = fb.get('/dogs', {"a": "a"})
+        print dogs
+        #for dog in dogs:
+        #    print dogs[dog]
