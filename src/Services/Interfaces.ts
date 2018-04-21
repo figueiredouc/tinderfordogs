104bab6599ae7e86937fea52d63056544d20b2b6
Daniela Guardado <dmguardado@ipn.pt>
2018-04-21 04:56:26 +0100

    FormBuilder in Users and Animal

---
+++ src/Services/Interfaces.ts
@@ -0,0 +1,24 @@
+export interface Animal {
+    name: string;
+    breederName?: string;
+    ownerName?: string;
+    sex?: string;
+    birthDate?: string;
+    landOfBirth?: string;
+    size?: string;
+    weigth?: string;
+    color?: string;
+    titles?: string;
+    features?: string;
+}
+
+export interface User {
+    name?: string;
+    username: string;
+    contact?: string;
+    city?: string;
+    address?: string;
+    isBreeder?: false,
+    latitude?: string;
+    longitude?: string;
+}
--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { User } from '../../Services/Interfaces';
 
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 /**
@@ -16,20 +18,22 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 
 export class UserPage {
-  user: any = {
-    name: null,
-    username: null,
-    contact: null,
-    city: null,
-    address: null,
-    isBreeder: false,
-    latitude: null,
-    longitude: null,
-  }
-  api = "http://10.128.166.165:5000";
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+  api = "http://10.128.166.165:5000";
+  private user: FormGroup;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, private formBuilder: FormBuilder ) {
+    this.user = this.formBuilder.group({
+      name: ['', Validators.required],
+      username: [''],
+      contact: [''],
+      city: [''],
+      address: [''],
+      isBreeder: [''],
+      latitude: [''],
+      longitude: [''],
+    });
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserPage');
@@ -37,12 +41,12 @@ export class UserPage {
 
   addUser(user) {
     return new Promise((resolve, reject) => {
-      this.http.post(this.api + '/create_user', JSON.stringify(this.user), {headers: "Content-Type", "application/json"})
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => {
-          reject(err);
-        });
-    });
+    //   this.http.post(this.api + '/create_user', JSON.stringify(this.user), {headers: "Content-Type", "application/json"})
+    //     .subscribe(res => {
+    //       resolve(res);
+    //     }, (err) => {
+    //       reject(err);
+    //     });
+     });
   }
 }
